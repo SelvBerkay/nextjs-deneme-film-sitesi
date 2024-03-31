1408441ab047d0144c089df43b970a997b48666a
SelvBerkay <kolinnn348@gmail.com>
2024-03-31 05:23:22 +0300

    gereksiz kod kaldırıldı...

--- components/SelectedMovie.js
+++ components/SelectedMovie.js
@@ -14,7 +14,6 @@ const SelectedMovie = ({ selectedMovie }) => {
         <h1 className='text-3xl font-bold'>{selectedMovie.title} - {selectedMovie.release_date}</h1>
         <p className='flex items-center text-amber-600 gap-2'><FaImdb size={25} color='orange' /> {selectedMovie.vote_average.toFixed(2)} ({selectedMovie.vote_count})</p>
         <p className='text-md '>{selectedMovie.overview.slice(0, 200)}...</p>
-        {selectedMovie.genres?.map(genre => <h3 key={genre.id}>{genre.name}</h3>)}
       </div>
       <Image fill src={`https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path}`} alt='' />
     </div>
