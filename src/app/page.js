f2d1a62b80cbfa2205f6b4eab316159dfcf30e9c
SelvBerkay <kolinnn348@gmail.com>
2024-04-02 04:52:05 +0300

    anasayfa düzenlendi

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex bg-black bg-opacity-50 w-screen h-screen items-center justify-center">
-      <h1 className='text-5xl'>WELCOME TO <span className='text-yellow-500 font-bold'>NEW MOVIE</span></h1>
+    <div className="flex flex-col bg-black bg-opacity-50 w-screen h-screen justify-center items-center gap-5">
+      <h1 className='text-3xl font-bold text-white'>Which type would you like to watch? Select one.</h1>
+      <div className='flex items-center justify-around w-full'>
+        <Link href={"/movies?genre=popular"} className='p-36 bg-black bg-opacity-50 rounded-lg hover:bg-opacity-100 transition-opacity text-white font-bold text-3xl cursor-pointer'>Movies</Link>
+        <Link href={"/series?genre=popular"} className='p-36 bg-black bg-opacity-50 rounded-lg hover:bg-opacity-100 transition-opacity text-white font-bold text-3xl cursor-pointer'>Series</Link>
+      </div>
     </div>
   )
 }
